48700e919605362b59adabb57fdebc1dd4f5443f
Fachu2989 <Fachu290589@gmail.com>
2022-10-30 21:31:18 -0300

    Update urls.py

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -5,11 +5,9 @@ from .views import curso, cursoFormulario,lista_curso,cursos, estudiante, inicio
 
 urlpatterns = [
     path("agrega/<nombre>/<camada>", curso),
-    path("lista-cursos/", lista_curso),
     path("cursos/", cursos, name="Cursos"),
     path("profesores/", profesores, name="Profesores"),
     path("estudiantes/", estudiante, name="Estudiantes"),
     path("", inicio, name="Inicio" ),
-    path("lista-cursos/", lista_curso),
     path("cursoFormulario/", cursoFormulario, name="cursoFormulario"),
 ]
