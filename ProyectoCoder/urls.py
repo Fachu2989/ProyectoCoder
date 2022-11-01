8642dc1c1515ca5cbee679f00f3ccaafaddd1b7f
Fachu2989 <Fachu290589@gmail.com>
2022-10-31 21:07:47 -0300

    Update urls.py

--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from AppCoder.views import cursoFormulario
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
